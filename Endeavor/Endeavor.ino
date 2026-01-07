0f9ebd8674bcec343e317b18c17f1108a443e2b3
David Wilson <dwilson@wilsonwebsite.com>
2026-01-06 22:00:31 -0500

    aa   12/14/2025 20:16 - Setting up WASP    12/19/2025 01:40 - ReWrite call for heat and turnoff upstairs    01/01/2026 02:39 - Turn valve all the time to heat upstairs    01/02/2026 02:36 - Pus to Endeavor    01/02/2026 02:45 - chgd env tempd    01/06/2026 21:45 - removed unused uncludes veryfi water flows add clearDisplay() to clear at begin

--- Endeavor/Endeavor.ino
+++ Endeavor/Endeavor.ino
@@ -63,7 +63,8 @@
 			12/19/2025 01:40 - ReWrite call for heat and turnoff upstairs
 			01/01/2026 02:39 - Turn valve all the time to heat upstairs
 			01/02/2026 02:36 - Pus to Endeavor
-			01/02/2026 02:45 - chgd env tempd 
+			01/02/2026 02:45 - chgd env tempd
+			01/06/2026 21:45 - removed unused uncludes veryfi water flows add clearDisplay() to clear at begin
 			
 
 
@@ -81,7 +82,6 @@
 //================================================================+======================
 //======================================================================================
 
-#include <gfxfont.h>
 #include <Adafruit_SPITFT_Macros.h>
 //#include <Adafruit_SPITFT.h>
 #include <Adafruit_GrayOLED.h>
@@ -89,7 +89,6 @@
 //#include <splash.h>
 #include <Adafruit_SSD1306_EMULATOR.h>
 #include <HttpClient.h>
-#include <b64.h>
 const char* hostName = "ENDEAVOR_12";
 uint8_t ON = 0x0;
 uint8_t OFF = 0x1;
@@ -146,7 +145,7 @@ int envLowTemp = 64; // 68 current Lo for LR kick on at this var
 int envLowTempOffSet = 0; // 0 offset
 
 
-int boilerHighTemp = 980; // 980 top temp for boiler
+int boilerHighTemp = 850; // 980 top temp for boiler
 int boilerHighTempOffSet = 0;
 
 int boilerLowTemp = 400; // 400 bottom temp for boiler
@@ -309,6 +308,7 @@ void disableEndeavor();
 void safetyCheck();
 void updateBurnTime(void);
 void updateDisplay();
+void clearDisplay();
 void turnOnBoiler(void);
 void turnOffBoiler(void);
 void blink();
@@ -485,6 +485,7 @@ void setup()
 	Serial.println(WiFi.macAddress());
 
 
+	clearDisplay();
 }
 
 //======================================================================================
@@ -683,6 +684,39 @@ void runMaintenance()
 	delay(10);
 	if (callForHeat) turnOnWater();
 	else turnOffWater();
+
+
+
+}
+
+
+
+
+void clearDisplay()
+{
+	ArduinoOTA.handle();
+
+	displayOneLineOne = ""; 
+	displayOneLineTwo = "";
+	displayOneLineThree = ""; 
+
+	// Display 1
+
+	displayOne.clearDisplay();
+	displayOne.setTextColor(1);
+	displayOne.setTextSize(1);
+	displayOne.setTextColor(WHITE);
+
+	displayOne.setCursor(11, 1);
+	displayOne.println(displayOneLineOne);
+
+	displayOne.setCursor(11, 11);
+	displayOne.println(displayOneLineTwo);
+
+	displayOne.setCursor(11, 22);
+	displayOne.println(displayOneLineThree);
+
+	displayOne.display();
 
 }
 
